f46d21d71bb3575d2bd2d568651e9c0fa85c3c1c
axcionel <52538388+axcionel@users.noreply.github.com>
2026-02-12 14:02:15 -0500

    3

--- Vue Project/src/router/index.js
+++ Vue Project/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Draft from '@/views/Draft.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Draft,
+    }
+  ],
 })
 
 export default router
---
+++ Vue Project/src/views/Draft.vue
@@ -0,0 +1,37 @@
+<template lang="">
+    <h1>Draft Your Team</h1>
+    <li v-for="people in draftees" :key="people.name">
+        {{ people.name }} - ${{ people.price }}
+    </li>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const draftees = ref([
+  { name: "20 Piece McNuggets", price: 4.99, type: "sides" },
+  { name: "McChicken", price: 3.99, type: "sandwiches" },
+  { name: "Large Fries", price: 3.99, type: "sides" },
+  { name: "McFlurry", price: 4.39, type: "desserts" },
+  { name: "Big Mac", price: 6.99, type: "sandwiches" },
+  { name: "Snack Wrap", price: 2.99, type: "sides" },
+  { name: "Spicy McCrispy", price: 3.99, type: "sandwiches" },
+  { name: "Egg McMuffin", price: 3.99, type: "sandwiches" },
+  { name: "Sausage Burrito", price: 2.99, type: "sides" },
+  { name: "Filet-O-Fish", price: 3.99, type: "sandwiches" },
+  { name: "Vanilla Ice Cream", price: 2.99, type: "desserts" },
+  { name: "Apple Pie", price: 2.99, type: "desserts" },
+  { name: "Happy Meal", price: 6.99, type: "combos" },
+  { name: "Quarter Pounder", price: 3.99, type: "sandwiches" },
+  { name: "Sausage McMuffin with Egg Meal", price: 6.99, type: "combos" },
+  { name: "Big Mac Meal", price: 7.99, type: "combos" },
+  { name: "Spicy Deluxe McCrispy Meal", price: 7.99, type: "combos" },
+  { name: "Spicy Snack Wrap Meal", price: 5.99, type: "combos" },
+  { name: "Sausage Biscuit with Egg", price: 3.99, type: "sandwiches" },
+  { name: "Bacon Egg and Cheese McGriddle", price: 3.99, type: "sandwiches" }
+]);
+</script>
+
+
+<style lang="">
+
+</style>
