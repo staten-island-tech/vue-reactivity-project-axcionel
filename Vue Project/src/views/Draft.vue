a91330dd64600333b0d24bd9f77f909864baa988
axcionel <52538388+axcionel@users.noreply.github.com>
2026-02-13 14:01:36 -0500

    Update Draft.vue

--- Vue Project/src/views/Draft.vue
+++ Vue Project/src/views/Draft.vue
@@ -1,33 +1,45 @@
 <template lang="">
-    <h1>Draft Your Team</h1>
-    <li v-for="people in draftees" :key="people.name">
-        {{ people.name }} - ${{ people.price }}
+    <h1>Draft</h1>
+    <li v-for="player in draftees" :key="player.name">
+        {{ player.name }} - {{ player.position }} - ${{ player.salary }} <img :src="player.image" alt="" />
     </li>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 const draftees = ref([
-  { name: "20 Piece McNuggets", price: 4.99, type: "sides" },
-  { name: "McChicken", price: 3.99, type: "sandwiches" },
-  { name: "Large Fries", price: 3.99, type: "sides" },
-  { name: "McFlurry", price: 4.39, type: "desserts" },
-  { name: "Big Mac", price: 6.99, type: "sandwiches" },
-  { name: "Snack Wrap", price: 2.99, type: "sides" },
-  { name: "Spicy McCrispy", price: 3.99, type: "sandwiches" },
-  { name: "Egg McMuffin", price: 3.99, type: "sandwiches" },
-  { name: "Sausage Burrito", price: 2.99, type: "sides" },
-  { name: "Filet-O-Fish", price: 3.99, type: "sandwiches" },
-  { name: "Vanilla Ice Cream", price: 2.99, type: "desserts" },
-  { name: "Apple Pie", price: 2.99, type: "desserts" },
-  { name: "Happy Meal", price: 6.99, type: "combos" },
-  { name: "Quarter Pounder", price: 3.99, type: "sandwiches" },
-  { name: "Sausage McMuffin with Egg Meal", price: 6.99, type: "combos" },
-  { name: "Big Mac Meal", price: 7.99, type: "combos" },
-  { name: "Spicy Deluxe McCrispy Meal", price: 7.99, type: "combos" },
-  { name: "Spicy Snack Wrap Meal", price: 5.99, type: "combos" },
-  { name: "Sausage Biscuit with Egg", price: 3.99, type: "sandwiches" },
-  { name: "Bacon Egg and Cheese McGriddle", price: 3.99, type: "sandwiches" }
+  { name: "Cam Ward", salary: 49000000, position: "QB", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4688380.png&w=350&h=254" },
+  { name: "Travis Hunter", salary: 47000000, position: "WR", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685415.png&w=350&h=254" },
+  { name: "Abdul Carter", salary: 45000000, position: "DE", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4725996.png&w=350&h=254" },
+  { name: "Will Campbell", salary: 44000000, position: "OT", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685298.png&w=350&h=254" },
+  { name: "Mason Graham", salary: 41000000, position: "DT", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4873232.png&w=350&h=254" },
+  { name: "Ashton Jeanty", salary: 36000000, position: "RB", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4890973.png&w=350&h=254" },
+  { name: "Armand Membou", salary: 32000000, position: "OT", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4789907.png&w=350&h=254" },
+  { name: "Tetairoa McMillan", salary: 28000000, position: "WR", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685472.png&w=350&h=254" },
+  { name: "Kelvin Banks Jr.", salary: 28000000, position: "OT", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685260.png&w=350&h=254" },
+  { name: "Colston Loveland", salary: 27000000, position: "TE", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4723086.png&w=350&h=254" },
+  { name: "Mykel Williams", salary: 25000000, position: "DE", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685623.png&w=350&h=254" },
+  { name: "Tyler Booker", salary: 23000000, position: "G", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685263.png&w=350&h=254" },
+  { name: "Kenneth Grant", salary: 22000000, position: "DT", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4894654.png&w=350&h=254" },
+  { name: "Tyler Warren", salary: 21000000, position: "TE", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4431459.png&w=350&h=254" },
+  { name: "Jalon Walker", salary: 21000000, position: "LB", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685597.png&w=350&h=254" },
+  { name: "Walter Nolen", salary: 19000000, position: "DT", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685503.png&w=350&h=254" },
+  { name: "Shemar Stewart", salary: 19000000, position: "DE", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685562.png&w=350&h=254" },
+  { name: "Grey Zabel", salary: 18000000, position: "G", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4693346.png&w=350&h=254" },
+  { name: "Emeka Egbuka", salary: 18000000, position: "WR", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4567750.png&w=350&h=254" },
+  { name: "Jahdae Barron", salary: 18000000, position: "CB", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4430925.png&w=350&h=254" },
+  { name: "Derrick Harmon", salary: 18000000, position: "DT", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4682980.png&w=350&h=254" },
+  { name: "Omarion Hampton", salary: 18000000, position: "RB", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685382.png&w=350&h=254" },
+  { name: "Matthew Golden", salary: 18000000, position: "WR", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4701936.png&w=350&h=254" },
+  { name: "Donovan Jackson", salary: 17000000, position: "G", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4431553.png&w=350&h=254" },
+  { name: "Jaxson Dart", salary: 17000000, position: "QB", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4689114.png&w=350&h=254" },
+  { name: "James Pearce Jr.", salary: 17000000, position: "DE", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5081394.png&w=350&h=254" },
+  { name: "Malaki Starks", salary: 17000000, position: "S", image: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685530.png&w=350&h=254" },
+  { name: "Tyleik Williams", salary: 16000000, position: "DT", image: "" },
+  { name: "Josh Conerly Jr.", salary: 16000000, position: "OT", image: "" },
+  { name: "Maxwell Hairston", salary: 15000000, position: "CB", image: "" },
+  { name: "Jihaad Campbell", salary: 15000000, position: "LB", image: "" },
+  { name: "Josh Simmons", salary: 15000000, position: "OT", image: "" }
 ]);
 </script>
 
